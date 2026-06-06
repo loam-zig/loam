876276c9fa044302daf7d465d12925a17079cdf6
devenjarvis <devenjarvis@gmail.com>
2026-06-06 06:53:19 -0400

    chore: Get versions managed and make microzig a lazy dependency so users don't have to download it

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     });
 
     // A test firmware that compiles loam as a consumer would
-    const mz_dep = b.dependency("microzig", .{});
+    const mz_dep = b.lazyDependency("microzig", .{}) orelse return;
     const mb = MicroBuild.init(b, mz_dep) orelse return;
 
     const check_fw = mb.add_firmware(.{
